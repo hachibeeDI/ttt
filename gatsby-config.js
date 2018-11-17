1ea0755885f7493a3c8228024770a1bd6d4b1a38
OGURA_Daiki <8hachibee125@gmail.com>
2018-11-17 17:23:59 +0900

    fs query makes more generic

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/src/ql-fs-sources/`,
       },
     },
     'gatsby-transformer-sharp',
@@ -23,7 +23,7 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/ql-fs-sources/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
