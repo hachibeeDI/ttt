3cc60c3d87b3123c025bbba649ab36576747c118
OGURA_Daiki <8hachibee125@gmail.com>
2018-11-17 14:39:09 +0900

    add new page templates

---
+++ src/pages/business.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Link} from 'gatsby';
+
+import Layout from '../components/layout';
+
+export default function Business() {
+  return (
+    <Layout>
+      <h1>Business desu</h1>
+    </Layout>
+  );
+}
---
+++ src/pages/profile.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Link} from 'gatsby';
+
+import Layout from '../components/layout';
+
+export default function Profile() {
+  return (
+    <Layout>
+      <h1>Profile</h1>
+    </Layout>
+  );
+}
